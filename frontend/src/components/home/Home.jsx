d44b6f8e73c496f28657061f508c6801ea3adb02
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-04-21 18:17:26 +0200

    .

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useStore } from "../../zustand/store";
 import { useNavigate } from "react-router-dom";
+import NavbarTop from "../navbars/NavTop";
 
 const HomePage = () => {
   const logState = useStore((state) => state.logState);
@@ -31,24 +32,25 @@ const HomePage = () => {
   logState();
 
   return (
-    <>
-      <div className="d-flex w-100 justify-content-between">
-        <h1>Home Page</h1>
-        <h2>Welcome {currentUser.username}</h2>
+    <div className="main-section-one">
+      <NavbarTop />
+
+      <div className="">
+        <h2 className="test">Welcome {currentUser.username}</h2>
         <button onClick={handleLogout}>Logout</button>
       </div>
-      <div className="d-flex">
-        <div className="m-5">
+      <div className="">
+        <div className="">
           <h1>users</h1>
           {users && users.length > 0 ? (
             <ul>
               {users.map((user) => (
-                <li key={user._id}>
+                <ul key={user._id}>
                   {user.username}{" "}
                   <button onClick={() => createGame(user._id, navigate)}>
                     Create Game
                   </button>
-                </li>
+                </ul>
               ))}
             </ul>
           ) : (
@@ -56,21 +58,17 @@ const HomePage = () => {
           )}
         </div>
 
-        <div className="m-5">
-          <h1>Offline users</h1>
-          <ul>offline:</ul>
-        </div>
-        <div className="m-5">
+        <div className="">
           <h1>Current Games</h1>
           {games && games.length > 0 ? (
             <ul>
               {games.map((game) => (
-                <li key={game._id}>
+                <ul key={game._id}>
                   {getOpponentUsername(game, currentUserId)}
                   <button onClick={() => navigate(`/game/${game._id}`)}>
                     Join Game
                   </button>
-                </li>
+                </ul>
               ))}
             </ul>
           ) : (
@@ -78,7 +76,7 @@ const HomePage = () => {
           )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ frontend/src/components/navbars/NavTop.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavbarTop = () => {
+  return (
+    <nav className="nav">
+      <div className="navbar-brand">
+        <a href="/home">
+          <img src="" alt="logo" className="logo"></img>
+        </a>
+      </div>
+      <nav classname="nav-link">Home</nav>
+    </nav>
+  );
+};
+
+export default NavbarTop;
